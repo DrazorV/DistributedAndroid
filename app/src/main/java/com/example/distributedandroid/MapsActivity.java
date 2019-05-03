4f9f0da10b8ab1a0223e9e61389e7161a7623295
DrazorV <vagelisp.97@gmail.com>
2019-05-03 13:03:19 +0300

    Commit

--- app/src/main/java/com/example/distributedandroid/MapsActivity.java
+++ app/src/main/java/com/example/distributedandroid/MapsActivity.java
@@ -3,7 +3,6 @@ package com.example.distributedandroid;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
-import android.location.Address;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -13,28 +12,26 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     ArrayList<Topic> topics;
     private volatile static HashMap<String, ArrayList<Topic>> hashed;
-    private static Scanner input = new Scanner(System.in);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,105 +51,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney = new LatLng(27.746974, 85.301582);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Kathmandu, Nepal"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.9940527,23.7324592),13f));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) return;
         mMap.setMyLocationEnabled(true);
     }
 
     public void onMapSearch(View view){
         EditText locationSearch = findViewById(R.id.editText);
         String location = locationSearch.getText().toString();
-        List<Address> addressList = null;
         String broker = null;
+
         Thread t = new Thread(() -> hashed = BroUtilities.MD5(topics));
         t.start();
         try {
             t.join();
-            for(Topic topic : topics) {
-                if(topic.getLineId().equals(location.trim())) {
-                    for(Map.Entry<String, ArrayList<Topic>> hash : hashed.entrySet()){
-                        for(Topic topic1 : hash.getValue()){
-                            if(topic1.getLineId().equals(topic.getLineId())) {
-                                broker = hash.getKey();
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                }
-            }
+            for(Topic topic : topics)
+                if(topic.getLineId().equals(location.trim()))
+                    for(Map.Entry<String, ArrayList<Topic>> hash : hashed.entrySet())
+                        for(Topic topic1 : hash.getValue())
+                            if(topic1.getLineId().equals(topic.getLineId())) broker = hash.getKey();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(broker);
-
         int port = 0;
         if (broker.equals("BrokerA")) port = 4322;
         if (broker.equals("BrokerB")) port = 5432;
         if (broker.equals("BrokerC")) port = 7654;
-
-
-        Thread thread = new Thread(new connection(port));
-        thread.start();
-
-        // location == busline
-        // if busline exists get last spot from bus
-//        if (location != null || !location.equals("")) {
-//            Geocoder geocoder = new Geocoder(this);
-//            try {
-//                addressList = geocoder.getFromLocationName(location, 1);
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println(addressList.toString());
-//            //mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-//            //mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-//        }
-    }
-
-
-    public class connection implements Runnable{
-
-        private int finalPort;
-
-        connection(int finalPort) {
-            this.finalPort = finalPort;
-        }
-
-        @Override
-        public void run() {
-            boolean temp = true;
-            while (temp) {
-                try (Socket clientSocket = new Socket("10.0.2.2", finalPort)) {
-                    ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-                    out.writeObject("Consumer");
-                    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    String line;
-                    while (temp) {
-                        PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
-                        System.out.println("Type the bus lines you re interested in or type 'bye' to change broker.");
-                        String busline = input.nextLine();
-                        if (!busline.toLowerCase().equals("bye")) {
-                            outToServer.println(busline);
-                            String answer = inFromServer.readLine();
-                            while (!answer.equals("next")) {
-                                System.out.println(answer);
-                                answer = inFromServer.readLine();
-                            }
-                            line = inFromServer.readLine();
-                        } else {
-                            temp = false;
-                        }
+        int finalPort = port;
+        Thread thread = new Thread( () ->{
+            try (Socket clientSocket = new Socket("10.0.2.2", finalPort)) {
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                out.writeObject("Consumer");
+                PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+                outToServer.println(location);
+                ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
+                HashMap<String, Value> answer = (HashMap<String, Value>) inFromServer.readObject();
+                runOnUiThread(() -> {
+                    for (Map.Entry<String, Value> answer_1: answer.entrySet()){
+                        MarkerOptions m = new MarkerOptions()
+                                .position(new LatLng(answer_1.getValue().getLatitude(),answer_1.getValue().getLongitude()))
+                                .snippet(answer_1.getValue().getBus().getTime().toLocaleString())
+                                .title(answer_1.getValue().getBus().getLineName() + " [" + answer_1.getValue().getBus().getBuslineId()+ "]")
+                                .infoWindowAnchor(1,1)
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
+                        mMap.addMarker(m);
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                });
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
             }
-        }
+        });
+        thread.start();
     }
 }
