e8400f9576526516e216f89b9deaa75b9500d090
DrazorV <vagelisp.97@gmail.com>
2019-05-04 12:52:29 +0300

    Commit

--- app/src/main/java/com/example/distributedandroid/MapsActivity.java
+++ app/src/main/java/com/example/distributedandroid/MapsActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
@@ -57,58 +58,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void onMapSearch(View view) throws InterruptedException {
+        Toast toast = Toast.makeText(MapsActivity.this,"Couldn't find connection with server!",Toast.LENGTH_SHORT);
+        final int[] port = {0};
+        toast.setGravity(Gravity.CENTER, 0, 0);
         EditText locationSearch = findViewById(R.id.editText);
         String location = locationSearch.getText().toString();
         final String[] broker = {null};
-
-        Thread t = new Thread(() -> hashed = BroUtilities.MD5(topics));
-        t.start();
-        t.join();
+        broker[0] = "brokerErr";
         Thread t2 = new Thread( () -> {
+            hashed = BroUtilities.MD5(topics);
             for (Topic topic : topics) {
                 if (topic.getLineId().equals(location.trim())) {
                     for (Map.Entry<String, ArrayList<Topic>> hash : hashed.entrySet()) {
                         for (Topic topic1 : hash.getValue()) {
-                            if (topic1.getLineId().equals(topic.getLineId()))
+                            if (topic1.getLineId().equals(topic.getLineId())){
                                 broker[0] = hash.getKey();
+                                break;
+                            }
                         }
                     }
+                    break;
                 }
             }
         });
         t2.start();
         t2.join();
-        int port = 0;
-        if (broker[0].equals("BrokerA")) port = 4322;
-        if (broker[0].equals("BrokerB")) port = 5432;
-        if (broker[0].equals("BrokerC")) port = 7654;
-        int finalPort = port;
+        if (broker[0].equals("brokerErr")) runOnUiThread(toast::show);
+        if (broker[0].equals("BrokerA")) port[0] = 4322;
+        if (broker[0].equals("BrokerB")) port[0] = 5432;
+        if (broker[0].equals("BrokerC")) port[0] = 7654;
         Thread thread = new Thread( () ->{
-            try (Socket clientSocket = new Socket("10.0.2.2", finalPort)) {
+            try (Socket clientSocket = new Socket("10.0.2.2", port[0])) {
                 ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
                 out.writeObject("Consumer");
                 PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
                 outToServer.println(location);
                 ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
                 HashMap<String, Value> answer = (HashMap<String, Value>) inFromServer.readObject();
-                runOnUiThread(() -> {
-                    if(answer == null){
-                        Toast toast = Toast.makeText(MapsActivity.this,"We couldn't find what you asked for, we may experience a problem with our servers",Toast.LENGTH_SHORT);
-                        toast.setGravity(Gravity.CENTER, 0, 0);
-                        toast.show();
-                    }else{
-                        for (Map.Entry<String, Value> answer_1: answer.entrySet()){
-                            MarkerOptions m = new MarkerOptions()
-                                    .position(new LatLng(answer_1.getValue().getLatitude(),answer_1.getValue().getLongitude()))
-                                    .snippet(answer_1.getValue().getBus().getTime().toLocaleString())
-                                    .title(answer_1.getValue().getBus().getLineName() + " [" + answer_1.getValue().getBus().getBuslineId()+ "]")
-                                    .infoWindowAnchor(1,1);
-                            mMap.addMarker(m);
-                        }
+                if(answer == null){
+                    toast.setText("We couldn't find what you asked for, we may experience a problem with our servers");
+                    runOnUiThread(toast::show);
+                }else{
+                    for (Map.Entry<String, Value> answer_1: answer.entrySet()){
+                        MarkerOptions m = new MarkerOptions()
+                                .position(new LatLng(answer_1.getValue().getLatitude(),answer_1.getValue().getLongitude()))
+                                .snippet(answer_1.getValue().getBus().getTime().toLocaleString())
+                                .title(answer_1.getValue().getBus().getLineName() + " [" + answer_1.getValue().getBus().getBuslineId()+ "]")
+                                .infoWindowAnchor(1,1);
+                        runOnUiThread(() -> mMap.addMarker(m));
                     }
-                });
+                }
+                toast.setText("Choose the preferred bus for more info!");
+                runOnUiThread(toast::show);
             } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
+                toast.setText("Couldn't find connection with server!");
+                runOnUiThread(toast::show);
             }
         });
         thread.start();
